ce3a68e4a4a72f08d7884f440089dce417824db1
Aleksey Marov <marov.a@raidixstorage.com>
2017-04-27 13:22:46 +0300

    Change nodes order to natural sort order

--- main.py
+++ main.py
@@ -17,18 +17,22 @@ class Node:
 def run_cmd(cmd_string, retry_on_failure_cnt):
     p = Popen(cmd_string, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
     out, err = p.communicate()
-    print(out)
-    print(err)
+    out_str = out.decode("utf-8")
+    err_str = err.decode("utf-8")
+    print(out_str)
+    print(err_str)
     i = 1
 
-    if(err != "" and retry_on_failure_cnt > 0):
+    if (err_str != "" and retry_on_failure_cnt > 0):
         print("Try again cmd (" + cmd_string + ") retry_cnt: " + str(retry_on_failure_cnt))
         retry_on_failure_cnt -= 1
         time.sleep(2)
         i += 1
         run_cmd(cmd_string, retry_on_failure_cnt)
-    #if
-    #print("Cmd execution done")
+
+    if (err_str != "" and retry_on_failure_cnt == 0):
+        print("Cmd (" + cmd_string + ") execution failed")
+
 
 def reset_nvme(node_ip_addr, dev):
     cmd = "sshpass ssh root@" + \
@@ -46,7 +50,7 @@ def reset_nvme(node_ip_addr, dev):
 
 def reset_all_nodes_nvme(nodes):
     #add hdms scan before rest to defaults
-    for name, node in nodes.items():
+    for name, node in natsort.natsorted(nodes.items()):
         print("Reseting all NVME on node: " + name + " ...")
         reset_nvme(node.ip_addr, "/dev/nvme0")
         reset_nvme(node.ip_addr, "/dev/nvme1")
@@ -69,7 +73,7 @@ def delete_namespaces(node_ip_addr, dev):
     call(cmd, shell=True)
 
 def delete_all_nodes_namespaces(nodes):
-    for name, node in nodes.items():
+    for name, node in natsort.natsorted(nodes.items()):
         print("Delete all NVME namespaces on node: " + name + " ...")
         delete_namespaces(node.ip_addr, "/dev/nvme0")
         delete_namespaces(node.ip_addr, "/dev/nvme1")
@@ -118,7 +122,7 @@ def create_all_nodes_namespaces(nodes):
 
     ns_size = NVME_SIZE / ((num_nodes + 1) / 2)
 
-    for name, node in nodes.items():
+    for name, node in natsort.natsorted(nodes.items()):
         print("Create NVME namespaces on node: " + name + " ...")
         create_namespaces(node.ip_addr, ns_size, num_nodes)
         print("Create namespaces done")
@@ -152,16 +156,16 @@ def create_nvmf_target(node):
     print(cmd)
     call(cmd, shell=True)
 
-    cmd = "sshpass ssh root@" + \
-          node.ip_addr + " 'echo 4096 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages'"
-
-    print(cmd)
-    call(cmd, shell=True)
-
-    cmd = "sshpass ssh root@" + \
-          node.ip_addr + " 'echo 4096 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages'"
-    print(cmd)
-    call(cmd, shell=True)
+    # cmd = "sshpass ssh root@" + \
+    #       node.ip_addr + " 'echo 4096 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages'"
+    #
+    # print(cmd)
+    # call(cmd, shell=True)
+    #
+    # cmd = "sshpass ssh root@" + \
+    #       node.ip_addr + " 'echo 4096 > /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages'"
+    # print(cmd)
+    # call(cmd, shell=True)
 
     #firstly kill all possible tgts
     # cmd = "sshpass ssh root@" + node.ip_addr + \
@@ -180,7 +184,7 @@ def create_nvmf_target(node):
 
 def reset_spdk(nodes):
     #rescan using spdk
-    for name, node in nodes.items():
+    for name, node in natsort.natsorted(nodes.items()):
         cmd = "sshpass ssh root@" + node.ip_addr + \
               " \'/root/spdk/scripts/setup.sh reset\'"
 
@@ -195,7 +199,7 @@ def reset_spdk(nodes):
 
 
 def create_all_nodes_tgts(nodes):
-    for name, node in nodes.items():
+    for name, node in natsort.natsorted(nodes.items()):
         print("Create NVMF target on node: " + name + " ...")
         create_nvmf_target(node)
         time.sleep(5)
@@ -203,7 +207,7 @@ def create_all_nodes_tgts(nodes):
 
 
 def connect_nvmf_targets(cur_node, nodes):
-    for name, node in sorted(nodes.items()):
+    for name, node in natsort.natsorted(nodes.items()):
         #
         # cmd = "sshpass ssh root@" + cur_node.ip_addr + \
         #       " \' ping " + node.ib_addr1 + " -c 4\'"
@@ -268,14 +272,14 @@ def connect_nvmf_targets(cur_node, nodes):
         call(cmd, shell=True)
 
 def connect_all_nodes_tgts(nodes):
-    for name, node in sorted(nodes.items()):
+    for name, node in natsort.natsorted(nodes.items()):
         print("Connecting NVMF target on node: " + name + " ...")
         connect_nvmf_targets(node, nodes)
         print("Connecting NVMF target done")
 
 
 def disconnect_nvmf_targets(cur_node, nodes):
-    for name, node in sorted(nodes.items()):
+    for name, node in natsort.natsorted(nodes.items()):
 
         cmd = "sshpass ssh root@" + cur_node.ip_addr + \
               " \' nvme disconnect -n nqn.2016-06.io.spdk." + \
@@ -292,7 +296,7 @@ def disconnect_nvmf_targets(cur_node, nodes):
         call(cmd, shell=True)
 
 def disconnect_all_nodes_tgts(nodes):
-    for name, node in nodes.items():
+    for name, node in natsort.natsorted(nodes.items()):
         print("Disonnecting NVMF target on node: " + name + " ...")
         disconnect_nvmf_targets(node, nodes)
         print("disconnecting NVMF target done")
@@ -309,8 +313,11 @@ def create_zfs_vol(node, nodes, ns_shift):
               " 'lsblk | grep nvme | cut --delimiter=\" \" -f 1'"
     p = Popen(cmd , stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
     out, err = p.communicate()
-    #all_nvme = sorted(out.split("\n"))
-    all_nvme = natsort.natsorted(out.split("\n"))
+    out_str = out.decode("utf-8")
+    err_str = err.decode("utf-8")
+
+    all_nvme = out_str.split("\n")
+    all_nvme = natsort.natsorted(all_nvme)
     all_nvme.remove("")
 
     for i in range(0, len(all_nvme), num_nodes):
@@ -336,14 +343,14 @@ def create_zfs_vol(node, nodes, ns_shift):
 
 def create_all_nodes_zfs(nodes):
     ns_shift = 0
-    for name, node in nodes.items():
+    for name, node in natsort.natsorted(nodes.items()):
         print("Createing ZFS on node: " + name + " ...")
         create_zfs_vol(node, nodes, ns_shift)
         ns_shift += 1
         print("Creating ZFS done")
 
 def destroy_all_nodes_zfs(nodes):
-    for name, node in nodes.items():
+    for name, node in natsort.natsorted(nodes.items()):
         print("Destroy zpool on node: " + name + " ...")
         cmd = "sshpass ssh root@" + node.ip_addr + \
               " \' zpool destroy tank \'"
@@ -354,7 +361,7 @@ def destroy_all_nodes_zfs(nodes):
 
 def main():
     nodes = load_conf("nodes.conf")
-    #disconnect_all_nodes_tgts(nodes)
+    disconnect_all_nodes_tgts(nodes)
     #reset_all_nodes_nvme(nodes)
     #delete_all_nodes_namespaces(nodes)
     #create_all_nodes_namespaces(nodes)
@@ -362,8 +369,8 @@ def main():
     #create_all_nodes_tgts(nodes)
     #connect_all_nodes_tgts(nodes)
 
-    create_all_nodes_zfs(nodes)
-    #destroy_all_nodes_zfs(nodes)
+    #create_all_nodes_zfs(nodes)
+    destroy_all_nodes_zfs(nodes)
 
     #disconnect_all_nodes_tgts(nodes)
 
